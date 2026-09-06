935b69a1c28a377afe6f132ec512a2f1ca254b0d
Joud Al Thonaya <althonayanjoud@gmail.com>
2026-09-06 21:50:06 +0300

    Clean up comments in TestPaths.java
    Removed author information and comments from TestPaths.java

--- src/testing/TestPaths.java
+++ src/testing/TestPaths.java
@@ -4,13 +4,7 @@ import testing.PerfectSquare;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-//Name: Joud Bander Al Thonayan 
-//ID: 445008632 
-//Email: 445008632@pnu.edu.sa 
-//Section: 6C2 
-//Group: 4 
-//Contribution: Control Flow Graph analysis, Edge-Pair Coverage,  
-//            Test Paths identification, and TestPaths.java test class 
+
 public class TestPaths {
 
     @Test(expected = IllegalArgumentException.class)
